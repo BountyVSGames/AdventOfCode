e4722352fd92893476dd7c5386bea10f3f8b94e0
BountyVSGames <boudewijn@boudewijnwitteveen.com>
2025-12-01 15:57:01 +0100

    Completed Day One of AoC25

--- AdventOfCode25/Days/Header/DayOne.h
+++ AdventOfCode25/Days/Header/DayOne.h
@@ -11,5 +11,14 @@ public:
     void Initialize();
     void RunAssignment();
     void RunBonusAssignment();
+private:
+    std::vector<int> SafeTurnDirectionValues;
+
+    //Normal ruleset
+    int CountsSafePointerAtZero = 0;
+    //0x434C49434B ruleset
+    int CountsSafePointerPassedZero = 0;
+
+    void TurnSafe(int& safePointerLoc, int& safePointerDir);
 };
 
--- AdventOfCode25/Days/Source/DayOne.cpp
+++ AdventOfCode25/Days/Source/DayOne.cpp
@@ -7,15 +7,65 @@ DayOne::DayOne(std::vector<std::string> parsedFile) : Assignment(parsedFile)
 
 void DayOne::Initialize()
 {
+	for (size_t i = 0; i < ParsedFile.size(); i++)
+	{
+		int value = std::stoi(ParsedFile[i].substr(1));
+		const char dirChar = ParsedFile[i][0];
 
+		if (dirChar == 'R')
+		{
+			SafeTurnDirectionValues.push_back(value);
+			continue;
+		}
+
+		SafeTurnDirectionValues.push_back(-value);
+	}
 }
 
 void DayOne::RunAssignment()
 {
+	int safePointerLoc = 50;
+	int countsSafePointerAtZero = 0;
 
+	for (size_t i = 0; i < SafeTurnDirectionValues.size(); i++)
+	{
+		TurnSafe(safePointerLoc, SafeTurnDirectionValues[i]);
+
+		if (safePointerLoc == 0)
+		{
+			countsSafePointerAtZero++;
+		}
+	}
+
+	std::cout << "The turner has rested on 0 a total of " << countsSafePointerAtZero << " times" << std::endl;
 }
 
 void DayOne::RunBonusAssignment()
 {
+	std::cout << "The turner has been on 0 a total of " << CountsSafePointerPassedZero << " times" << std::endl;
+}
 
+void DayOne::TurnSafe(int& safePointerLoc, int& safePointerDir)
+{
+	int dir = safePointerDir > 0 ? 1 : -1;
+	int itCount = std::abs(safePointerDir);
+
+	for (size_t i = 0; i < itCount; i++)
+	{
+		safePointerLoc += dir;
+
+		if (safePointerLoc == 100)
+		{
+			safePointerLoc = 0;
+		}
+		else if (safePointerLoc == -1)
+		{
+			safePointerLoc = 99;
+		}
+
+		if (safePointerLoc == 0)
+		{
+			CountsSafePointerPassedZero++;
+		}
+	}
 }
